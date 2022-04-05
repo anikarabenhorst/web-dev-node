a1467a237769dfa94caba4b67da4457747d31a8c
anikarabenhorst <anikacchase@gmail.com>
2022-04-05 19:19:25 -0400

    through 5

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import helloController from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
 
 const app = express();
-
+app.use(express.json());
 helloController(app);
 userController(app);
+tuitsController(app);
 
 app.listen(4000);
