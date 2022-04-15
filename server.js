0a6af9761a0bd1a2aad02e2b2f971cf54d33c637
anikarabenhorst <anikacchase@gmail.com>
2022-04-15 16:03:42 -0400

    working on server changes

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ import helloController from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost:27017/webdev');
-
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev'
+mongoose.connect('mongodb+srv://anika:password123!@tuits.tikel.mongodb.net/Tuits?retryWrites=true&w=majority');
+// 'mongodb+srv://anika:password123!@tuits.tikel.mongodb.net/Tuits?retryWrites=true&w=majority'
 const app = express();
 app.use(cors());
 app.use(express.json());
